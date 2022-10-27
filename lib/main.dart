332e7e54debf44304d9c3b5554a2796bd670714d
islam zareer <187062@ppu.edu.ps>
2022-10-27 21:34:22 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BusniessCardApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class BusniessCardApp extends StatelessWidget {
+  BusniessCardApp({Key? key}) : super(key: key);
+  double radius = 112;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: SafeArea(
-            child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: const Text(
-              "Busniss",
-              style: TextStyle(fontWeight: FontWeight.bold),
+      home: Scaffold(
+        backgroundColor: const Color(0xFF2B475E),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+              radius: 112,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 110,
+                backgroundImage: AssetImage('images/person.jpg'),
+              ),
             ),
-          ),
-          body: Container(),
-        )));
+            Text(
+              'Islam Zareer',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+              ),
+            ),
+            Text(
+              'Computer Engineer',
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(
+              color: Color.fromRGBO(108, 128, 144, 1),
+              thickness: 1,
+              indent: 60,
+              endIndent: 60,
+              height: 10,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 32,
+                  color: Color.fromRGBO(43, 71, 94, 1),
+                ),
+                title: Text(
+                  '(+972) 592645602',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  size: 32,
+                  color: Color.fromRGBO(43, 71, 94, 1),
+                ),
+                title: Text(
+                  'Islamzareer@gmail.com',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:busniess_card/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( BusniessCardApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
